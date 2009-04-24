9c222c133129a0bedb18bbbe89a973084f1a0728
kernel01 <kernel01@3197f4be-8d26-0410-945d-dd56791a9a7e>
2009-04-24 18:34:59 +0000

    Simple shell script to install the phing and VersionControl_SVN PEAR modules and run the build-setup and dev-setup targets.

---
+++ pinf_bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+PEAR_GUESS=`which pear`
+if [ -n "$PEAR_GUESS" ]; then
+	PRINT_DEFAULT_PEAR="[$PEAR_GUESS]"
+fi
+echo "Welcome to the PINF bootstrapping script!"
+while [ ! -x "$PEAR_BIN" ]; do 
+	echo "Please provide a path to pear $PRINT_DEFAULT_PEAR:"
+	read PEAR_BIN
+	if [ -z $PEAR_BIN ];then
+		PEAR_BIN=$PEAR_GUESS
+	fi
+done
+if ! $PEAR_BIN info phing/phing 2>&1 >/dev/null; then
+	$PEAR_BIN channel-discover pear.phing.info 
+	if ! $PEAR_BIN install phing/phing;then 
+		echo "Failed to install phing which is required for PINF"
+		exit 1
+	fi
+fi
+if ! $PEAR_BIN info VersionControl_SVN 2>&1 >/dev/null; then
+	if ! $PEAR_BIN install channel://pear.php.net/VersionControl_SVN-0.3.1; then
+		echo "Failed to install VersionControl_SVN which is required for PINF"
+		exit 1
+	fi
+fi
+BIN_DIR=`$PEAR_BIN config-get bin_dir`
+$BIN_DIR/phing build-setup
+$BIN_DIR/phing dev-setup
