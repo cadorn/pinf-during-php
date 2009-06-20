d2e585ff6dc33a8ba275541da2ca24e2261e7851
christoph@christophdorn.com <christoph@christophdorn.com@3197f4be-8d26-0410-945d-dd56791a9a7e>
2009-06-20 18:33:55 +0000

    Fixed unit test

--- trunk/StandardLibrary/tests/functions/scandirRecursiveTest.php
+++ trunk/StandardLibrary/tests/functions/scandirRecursiveTest.php
@@ -14,13 +14,29 @@ class PINF_StandardLibrary_functions_scandirRecursiveTest extends PHPUnit_Framew
         $paths = scandir_recursive(dirname(__FILE__).DIRECTORY_SEPARATOR.'_files'.DIRECTORY_SEPARATOR.'DirTree1');
 
         // Strip temporary files
-        for( $i=sizeof($paths)-1 ; $i>0 ; $i-- ) {
+        $ignore_dirs = array();
+        for( $i=0 ; $i<sizeof($paths) ; $i++ ) {
             $path_info = pathinfo($paths[$i]);
-            if(substr($path_info['filename'],0,4)=='.tmp') {
-                array_splice($paths,$i,1);
-            } 
+            if(substr($path_info['basename'],0,4)=='.tmp' ||
+               $path_info['basename']=='.svn') {
+
+              array_splice($paths,$i,1);
+              $i--;
+                
+              if($path_info['basename']=='.svn') {
+                $ignore_dirs[] = (($path_info['dirname']!='.')?$path_info['dirname'].DIRECTORY_SEPARATOR:'') . $path_info['basename'];
+              }
+            } else {
+              foreach( $ignore_dirs as $ignore_dir ) {
+                
+                if(substr($path_info['dirname'],0,strlen($ignore_dir))==$ignore_dir) {
+                  array_splice($paths,$i,1);
+                  $i--;
+                }
+              }
+            }
         }
-        
+
         $paths_expected = array();
         $paths_expected[] = 'Dir1';
         $paths_expected[] = 'Dir1/Dir1';
